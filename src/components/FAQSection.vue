7ad1e56d31236d33a327252dca01a34dadd3159d
Lovasoa <maitrejedi@lovasoa.fr>
2021-09-17 00:37:00 +0200

    wording

--- src/components/FAQSection.vue
+++ src/components/FAQSection.vue
@@ -4,7 +4,7 @@
     <div class="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
       <div class="max-w-3xl mx-auto divide-y-2 divide-gray-200">
         <h2 class="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">
-          Frequently asked questions
+          Foire Aux Questions
         </h2>
         <dl class="mt-6 space-y-6 divide-y divide-gray-200">
           <Disclosure as="div" v-for="faq in faqs" :key="faq.question" class="pt-6" v-slot="{ open }">
