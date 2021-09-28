ff62a08c3c39204894d5d714b28d719d48a46301
Lovasoa <maitrejedi@lovasoa.fr>
2021-09-29 00:41:25 +0200

    feat: About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,44 +1,41 @@
 <template>
-  <main>
-    <div
-      class="px-4 py-6 mt-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
-    >
-      <div class="sm:text-center lg:text-left">
-        <h2
-          class="text-4xl font-extrabold leading-10 tracking-tight text-gray-900  sm:text-5xl sm:leading-none md:text-6xl"
-        >
-          L’histoire du Loma Club
-        </h2>
-        <p
-          class="mt-3 text-base text-gray-700  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-        >
-          J’ai 27 ans et je suis la maman de Paloma née en Janvier 2020. <br>
-          Avant le Loma Club j’ai réalisé deux séries documentaires <a target="_blank" rel="noopener" href="https://www.youtube.com/watch?v=6cyUCrBavpw&list=PLOWXUCzoqZ2LTEDo_it6NaT12EHP_fpBS">MEUFS</a> et <a target="_blank" rel="noopener" href="https://www.youtube.com/watch?v=UhDdx38Mk9k&list=PLOWXUCzoqZ2L7C5ayx7nBxcgJJ0QPcd3-">MERES</a>, toutes les deux disponibles sur YouTube. <br>
-          J’écris également autour de la femme, de nos corps et de la maternité. <br>
+  <main class="relative px-4 py-12 sm:px-6 lg:px-8">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1 class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">L’histoire du Loma Club</h1>
+      </div>
+      <div class="mt-6 prose prose-yellow prose-lg text-gray-700 mx-auto">
+        <p>
+          L’idée du Loma Club m’est venue quelques mois après avoir accouché. J’avais envie de pouvoir échanger avec d’autres femmes qui comprenaient ce que je traversais, tout en préservant un espace sans jugement où l’on peut tout se dire et tout entendre.
         </p>
-      </div>
-      <div class="sm:text-center lg:text-left">
-        <h2
-          class="text-4xl font-extrabold leading-10 tracking-tight text-gray-900  sm:text-5xl sm:leading-none md:text-6xl"
-        >
-          Qui est Josépha ?
-        </h2>
-        <p
-          class="mt-3 text-base text-gray-700  sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-        >
-          Le Loma Club est né un soir de mai 2020, quelques mois après ma fille Paloma.<br>
-          En devenant mère j’ai eu envie d’échanger avec d’autres femmes qui comprenaient ce que je vivais, je voulais des échanges plus profonds que ceux que l’on peut parfois avoir avec notre entourages ou avec les autres mères rencontrées sur Instagram. Je souhaitais un espace sans jugement où on peut tout se dire et où on peut tout entendre.<br>
-          J’ai eu l’idée du Loma Club un soir après avoir participé à un cercle de parole organisé par une doula. J’avais envie d’aller plus loin, de voir des profils différents de femmes, de pouvoir rencontrer des femmes qui ne participent pas à ces cercles là. A cette époque je regardais la série Working Mum qui m’a aussi beaucoup inspiré.<br>
-          Après plusieurs mois à animer des cercles de parole test avec des mamans volontaires et dont les retours m’ont été plus que bénéfiques, j’ai lancé le Loma Club en août 2020. Le bouche à oreille a fonctionné, le réseau s’est agrandit et nous voila un an plus tard.<br>
+        <p>
+          Le Loma Club a commencé par se réunir une fois par semaine, et face à l’engouement, le projet s’est rapidement développé. L’armée pacifique était née.
         </p>
+        <p>
+          Aujourd’hui le Loma Club compte plusieurs sessions par jour, sur différents thèmes, et ce du lundi au vendredi.
+        </p>
+        <p>
+          Bienvenue dans l’armée pacifique !
+        </p>
+        <div class="lg:grid lg:gap-x-12 lg:grid-rows-1 lg:grid-cols-5 lg:grid-flow-row-dense">
+          <div class="relative aspect-w-16 aspect-h-9 lg:row-start-1 lg:col-start-1 lg:col-span-2 lg:-translate-x-32">
+            <img class="absolute !m-0 inset-0 object-cover object-top rounded-lg" src="/img/JosephaRaphard.jpg" alt="Josépha Raphard créatrice du Loma club" width="1310" height="873" />
+          </div>
+          <div class="lg:row-start-1 lg:col-start-2 lg:col-span-4">
+            <h2>Qui est Josépha ?</h2>
+            <p>J’ai 27 ans et je suis la maman de Paloma née en Janvier 2020.</p>
+            <p>Après avoir été attachée de presse puis avoir réalisé deux séries documentaires MEUFS et MERES, disponibles sur YouTube, j’ai créé le Loma Club.</p>
+            <p>Je suis également autrice, j’écris principalement autour de la femme, de nos corps et de la maternité.</p>
+          </div>
+        </div>
+        <h2>Meufs & Méres</h2>
+        <p class="!mb-1">Onze femmes de 25 à 33 ans nous racontent leur vision "d'être une femme en 2018".</p>
+        <div class="mx-auto aspect-w-16 aspect-h-9 bg-pink-500">
+          <iframe src="https://www.youtube-nocookie.com/embed/6cyUCrBavpw?start=174s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <p class="!mb-1">Sept femmes de 28 à 35 ans nous confient leurs ressentis concernant leur maternité.</p>
+        <div class="mx-auto aspect-w-16 aspect-h-9 bg-pink-500">
+          <iframe class="mx-auto" src="https://www.youtube-nocookie.com/embed/UhDdx38Mk9k?start=5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </div>
-      <div class="sm:text-center lg:text-left">
-        <h2
-          class="text-4xl font-extrabold leading-10 tracking-tight text-gray-900  sm:text-5xl sm:leading-none md:text-6xl"
-        >
-          Meufs & Méres
-        </h2>
-      </div>
-    </div>
   </main>
 </template>
