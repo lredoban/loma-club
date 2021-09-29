7c86fcb00765681e546d6b264832e026bbdce5f6
Lovasoa <maitrejedi@lovasoa.fr>
2021-09-29 11:17:29 +0200

    feat(planning): design planing and sessions

---
+++ src/components/Session.vue
@@ -0,0 +1,58 @@
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: {
+    date: { 
+      type: Object,
+      required: true
+    },
+    session: { 
+      type: Object,
+      required: true
+    },
+    today: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const { date, session, today } = props
+    const ticketsLeft = computed(() => session.tickets_total - session.tickets_sold )
+    const hasTickets = computed(() => ticketsLeft.value > 0 )
+    const displayBookingInfos = today.isBefore(date) || today.isSame(date) && today.hour() < +(session.start_time.replace(':00', ''))
+
+    return {
+      displayBookingInfos,
+      hasTickets,
+      session,
+      ticketsLeft
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h3 class="font-semibold text-ocre tracking-wide">{{ session.title }}</h3>
+    <p class="font-semibold text-beige">{{ session.start_time }}</p>
+    <template v-if="displayBookingInfos">
+      <div class="text-sm">
+        <p v-if="hasTickets">Places restantes: {{ ticketsLeft }}</p>
+        <p v-else>Plus de place disponible</p>
+      </div>
+      <div class="mt-4 ">
+        <a :href="session.url" target="_blank" reel="noopener" class="relative inline-block py-0 px-5 rounded-full bg-ocre text-white tracking-wide uppercase hover:ring-2 hover:ring-offset-2 hover:ring-ocre">
+          <span>
+            {{
+              hasTickets ?
+                'Réserver' :
+                'Intégrer'
+            }}
+          </span>
+          <small v-if="!hasTickets" class="absolute text-ocre bottom-0 left-0 -mb-4 ml-3 text-xs lowercase">la liste d'attente</small>
+        </a>
+      </div>
+    </template>
+  </div>
+</template>
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,39 +1,96 @@
 <template>
-  <h2 class="text-2xl">
-    Le planning
-  </h2>
-  <ul v-if="sessions.length">
-    <li v-for="session in sessions" :key="session">
-      <div class="font-bold text-xl">{{ session.title }}</div>
-      <div>{{ new Date(session.start_date).toLocaleDateString('fr-FR', dateOptions) }}</div>
-      <div>{{ session.start_time }}</div>
-      <a :href="session.url">Réserver</a>
-    </li>
-  </ul>
+  <main class="mx-auto py-6 px-2 border-ocre md:px-6">
+    <h1 class="text-5xl text-center absolute opacity-0">
+      Le planning
+    </h1>
+    <div class="divide-y-2 divide-dashed divide-opacity-20 divide-ocre">
+      <div v-for="(week, i) in weeks" :key="i">
+        <ol class="my-12 grid grid-cols-2 gap-x-6 gap-y-16 justify-items-center md:grid-cols-3 lg:grid-cols-5">
+          <li v-for="d in week" :key="d.dayOfYear" :class="today.isAfter(d.date) && 'opacity-30'">
+            <div class="text-center font-semibold tracking-wide mb-6">{{ d.day }} {{ d.date.format('DD/MM') }}</div>
+            <div v-if="d.sessions.length === 0">Pas de session</div>
+            <div v-else class="space-y-10">
+              <Session v-for="session in d.sessions" :key="session.id" :session="session" :date="d.date" :today="today" />
+            </div>
+          </li>
+        </ol>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 import { onMounted, ref, unref } from 'vue'
+import { useRouter } from 'vue-router'
+import useUser from '../user'
+import * as dayjs from 'dayjs'
+import 'dayjs/locale/fr'
+import dayOfYear from 'dayjs/plugin/dayOfYear'
+// import sessionsJson from './sessions.json'
+
+import Session from '../components/Session.vue'
+
+dayjs.extend(dayOfYear)
+
+const daysList = [
+  'lundi',
+  'mardi',
+  'mercredi',
+  'jeudi',
+  'vendredi'
+]
 
 export default {
+  components: {
+    Session
+  },
   setup () {
-    const sessions = ref([])
+    const weeks = ref([])
     const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    const router = useRouter()
+    const { isAuth } = useUser()
+
+    const now = dayjs().locale('fr')
+    const seedWeek = offset => daysList.map((day, i) => {
+      const date = now.day(i + offset)
+      return {
+        day, date, dayOfYear: date.dayOfYear(), sessions: []
+      }
+    })
+    weeks.value.push(seedWeek(1))
+    weeks.value.push(seedWeek(8))
+
+
+
+    const seedSessions = sessions => {
+      sessions.forEach(session => {
+        const sessionDay = dayjs(session.start_date).dayOfYear()
+        console.warn(dayjs(session.start_date), sessionDay)
+        const dateObj = weeks.value.reduce((found, days) => found || days.find(day => day.dayOfYear === sessionDay), null)
+        if (!!dateObj) dateObj.sessions.push(session)
+      })
+    }
 
     onMounted(() => {
+      if (!isAuth.value) {
+        router.replace('/')
+        return
+      }
+      // seedSessions(sessionsJson)
       fetch('/.netlify/functions/fetch-sessions').then(res => {
         if (res.ok) {
           res.json().then(data => {
-            sessions.value = data.sessions.filter(e => e.status === 'Live')
-            console.warn(unref(sessions))
+            const filteredSessions = data.sessions.filter(e => e.status === 'Live')
+            seedSessions(filteredSessions)
           })
         }
       })
     })
 
     return {
-      sessions,
-      dateOptions
+      dateOptions,
+      weeks,
+      today: now
     }
   }
 }
