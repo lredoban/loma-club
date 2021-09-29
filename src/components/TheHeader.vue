dd396a360a07bb2dffb30390d80e8210321e32f8
Lovasoa <maitrejedi@lovasoa.fr>
2021-09-29 14:55:38 +0200

    feat(header): more dynamic with loggin/out

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -58,13 +58,16 @@
         </div>
         <div class="flex items-center space-x-4">
           <div class="hidden flex-shrink-0 md:block">
-            <router-link v-if="isAuth" to="/profil">Mon profil</router-link>
+            <router-link v-if="isAuth" to="/profil" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-ocre bg-white shadow-sm  ring-2 ring-ocre hover:bg-ocre hover:bg-opacity-10 focus:outline-none">Mon profil</router-link>
             <button v-else class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-ocre bg-white shadow-sm  ring-2 ring-ocre hover:bg-ocre hover:bg-opacity-10 focus:outline-none" @click.prevent="openLogin = true">
               Se connecter
             </button>
           </div>
           <div class="flex-shrink-0">
-            <a :href="formUrl" rel="noopener" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-ocre shadow-sm hover:bg-ocre hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-ocre">
+            <router-link v-if="isAuth" to="/planning" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-ocre shadow-sm hover:bg-ocre hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-ocre md:hidden">
+              Planning
+            </router-link>
+            <a v-else :href="formUrl" rel="noopener" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-ocre shadow-sm hover:bg-ocre hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-ocre">
               S'inscrire
             </a>
           </div>
@@ -93,10 +96,29 @@
             >{{ link.text }}</a
           >
         </router-link>
+        <router-link
+          v-if="isAuth"
+          to="/planning"
+          custom
+          v-slot="{ navigate, href, isExactActive }"
+        >
+          <a
+            :href="href"
+            @click="navigate"
+            class="block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+            :class="[
+              isExactActive
+                ? 'bg-gray-100 border-ocre text-ocre'
+                : 'border-transparent text-gray-700 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700'
+            ]"
+            >Planning</a
+          >
+        </router-link>
       </div>
       <div class="pt-4 pb-3 border-t border-gray-200">
         <div class="space-y-1">
-          <a href="#" class="block px-4 py-2 text-base font-medium text-gray-700 hover:text-gray-700 hover:bg-gray-100 sm:px-6" @click.prevent="openLogin = true">Se connecter</a>
+          <router-link  v-if="isAuth" to="/profil" class="block px-4 py-2 text-base font-medium text-gray-700 hover:text-gray-700 hover:bg-gray-100 sm:px-6">Mon profil</router-link>
+          <a v-else href="#" class="block px-4 py-2 text-base font-medium text-gray-700 hover:text-gray-700 hover:bg-gray-100 sm:px-6" @click.prevent="openLogin = true">Se connecter</a>
         </div>
       </div>
     </DisclosurePanel>
