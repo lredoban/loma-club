96d350d0b5878aae4cab9a4c1b58e25fa74aaea1
Lovasoa <maitrejedi@lovasoa.fr>
2021-11-14 00:14:09 +0100

    feat(fetch session): keep fetching eventzilal during transition

--- netlify/functions/fetch-sessions/fetch-sessions.js
+++ netlify/functions/fetch-sessions/fetch-sessions.js
@@ -4,11 +4,14 @@ const dayjs = require('dayjs')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
 dayjs.extend(customParseFormat)
 
-const tixomUrl = process.env.TIXOOM_URL
+const tixoomUrl = process.env.TIXOOM_URL
+const apiKey = process.env.EVENTZILLA_API_KEY
 
 const handler = async function () {
   try {
-    const { results: events } = await $fetch(tixomUrl)
+    const sessionsZilla = await fetchEventzilla()
+    
+    const { results: events } = await $fetch(tixoomUrl)
 
     const sessions = events
       .map(event => {
@@ -23,7 +26,7 @@ const handler = async function () {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ sessions })
+      body: JSON.stringify({ sessions: [...sessions, ...sessionsZilla] })
     }
   } catch (error) {
     // output to netlify function log
@@ -34,6 +37,35 @@ const handler = async function () {
       body: JSON.stringify({ msg: error.message }),
     }
   }
+}
+
+const fetchEventzilla = () => {
+  return new Promise(async (resolve) => {
+    const sessionsObj = {}
+    const { events_live } = await $fetch('https://www.eventzillaapi.net/api/v2/events?offset=0&limit=100&status=live', {
+      headers: { Accept: 'application/json', 'x-api-key': apiKey }
+    })
+    events_live.forEach(event => {
+      sessionsObj[event.id] = event
+    })
+    const { events } = await $fetch('https://www.eventzillaapi.net/api/v2/events?offset=0&limit=100', {
+      headers: { Accept: 'application/json', 'x-api-key': apiKey }
+    })
+    events.forEach(event => {
+        if (!sessionsObj[event.id]) sessionsObj[event.id] = event
+      })
+    resolve(Object.values(sessionsObj)
+      .sort((s1, s2) => s1.start_time > s2.start_time ? 1 : -1)
+      .map(s => ({
+        ...s,
+        topic: s.title,
+        showtime: s.start_date,
+        startTime: s.start_time,
+        availability: s.tickets_sold + 1
+      }))
+    )
+  })
+
 }
 
 module.exports = { handler }
