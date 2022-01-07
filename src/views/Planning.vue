42497f2ebf4b552395c3b45781e9e24c16b3328e
Lovasoa <maitrejedi@lovasoa.fr>
2022-01-07 12:44:39 +0100

    feat(planning): bring back warning

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -3,6 +3,9 @@
     <h1 class="text-5xl text-center absolute opacity-0">
       Le planning
     </h1>
+    <p v-if="!isAuth">
+      Veuillez vous connecter afin de pouvoir réserver une session.
+    </p>
     <p>
       Le planning affiche seulement les deux prochaines semaines. Il sera mis à jour tous les dimanches.
     </p>
@@ -32,6 +35,7 @@ import { onMounted, ref } from 'vue'
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 import dayOfYear from 'dayjs/plugin/dayOfYear'
+import useUser from '../user'
 // import sessionsJson from './sessions.json'
 
 import Session from '../components/Session.vue'
@@ -54,6 +58,7 @@ export default {
     const weeks = ref([])
     const sessions = ref([])
     const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    const { isAuth } = useUser()
 
     const now = dayjs().locale('fr')
     const seedWeek = offset => daysList.map((day, i) => {
@@ -91,6 +96,7 @@ export default {
 
     return {
       dateOptions,
+      isAuth,
       sessions,
       today: now,
       weeks
