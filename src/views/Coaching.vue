ec556bc6545d6f650fd7b96f13d7d5cf25589708
Lovasoa <maitrejedi@lovasoa.fr>
2022-09-15 11:25:20 +0200

    feat(coaching): update

--- src/views/Coaching.vue
+++ src/views/Coaching.vue
@@ -10,12 +10,14 @@
           </h1>
         </div>
       </div>
-      <div class="relative w-full p-6 sm:h-72 md:h-[48rem] lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+      <div class="relative w-full sm:h-72 md:h-[48rem] lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
         <div class="absolute inset-0 flex justify-center items-center">
           <div class="rounded-shadow w-full h-full rounded-full"></div>
         </div>
-        <img src="/img/Josepha.png" alt="Josepha lors d'un accompagnement individuel"
-          class="mix-blend-darken h-full transform scale-150" />
+        <div class="overflow-hidden w-full h-full">
+          <img src="/img/Josepha.png" alt="Josepha lors d'un accompagnement individuel"
+            class="mix-blend-darken h-auto md:h-full transform scale-150" />
+        </div>
       </div>
     </section>
     <main class="relative px-4 py-12 sm:px-6 lg:px-8">
@@ -24,52 +26,24 @@
           Qu’est ce que l’accompagnement individuel ?
         </h2>
         <p>
-          L’accompagnement individuel est un accompagnement sur mesure ouvert à toutes les guerrières pacifiques. <br />
-          <strong>
-            Lors de cet accompagnement nous formerons toi et moi une équipe, les suivis se font sur plusieurs mois ou
-            semaines et demandent un réel investissement de ta part afin que nous puissions rendre cet accompagnement le
-            plus performant possible.
-          </strong>
-          <br>
-          Quand tu sentiras que tout bouge autour de toi, que tu ne sauras plus comment recevoir les émotions de celles
-          et
-          ceux qui t’entourent et qu’il se passera mille choses dans ta tête et dans ton corps, nos rendez vous te
-          permettront de pouvoir te recentrer et d’avoir une écoute et des réactions de ma part solides et constantes.
-          Nous formerons toi et moi, une équipe aussi magique que puissante.
+          L’accompagnement individuel est un accompagnement <strong>haut de gamme, sur mesure,</strong> ouvert à toutes les guerrières pacifiques. Cet accompagnement peut se faire durant le post-partum mais aussi dans les années qui suivent.
+        </p>
+        <p>
+          Ce suivi te permet d'avoir un <strong>safe place et une réelle confidentialité,</strong> nous formerons toi et moi une puissante équipe.
         </p>
         <h2>Premier rendez vous </h2>
         <p>
-          Le premier rendez-vous nous permettra, à toi comme à moi, de nous choisir. Si nous voulons que notre équipe
-          fonctionne il est important que la magie opère dès le début entre nous. <strong>
-            Ce premier rendez-vous est gratuit et
-            n’engage à rien
-          </strong>, il permet de déterminer tes objectifs, tes envies, tes doutes, tes rêves et de voir ensemble ce
-          que je peux t’apporter.
+          Le premier rendez-vous nous permettra, à toi comme à moi, de nous choisir. <br/>
+          <strong>Les places sont limitées</strong> et pour que notre équipe fonctionne, il est important que la magie opère entre nous. 
         </p>
         <p>
-          Afin de prendre ce premier rendez-vous il est important de bien remplir ce formulaire : *lien vers le
-          questionnaire* , plus tu répondras de façon précise aux questions et plus je pourrais commencer à apprendre à
-          te
-          connaître. <br>
-          La durée et les tarifs de l’accompagnement individuel sont sur mesure.
+          <strong>Ce premier rendez-vous est gratuit et n’engage à rien,</strong> il permet de déterminer tes objectifs, tes doutes, tes attentes et de voir ensemble ce qu'on peut mettre en place. Afin de prendre ce premier rendez-vous il est important de bien remplir <a href="https://forms.gle/P3ihzWcRxNsYFTPL9" target="_blank" rel="noopener noreferrer">ce formulaire</a> , plus tu répondras de façon précise aux questions et plus je pourrais commencer à apprendre à te connaître. 
         </p>
         <h2>
-          L’histoire de l’accompagnement individuel :
+          Durée et tarifs
         </h2>
         <p>
-          Un soir après une session du Loma Club je reçois un message « Josépha j’ai adoré ce qu’il s’est passé lors des
-          dernières sessions, particulièrement ce que tu as pu me dire, et j’aurai aimé creuser ces idées avec toi. Est
-          ce
-          que tu fais des sessions Loma Club en tête à tête? »
-          Le Loma Club évolue chaque jour grâce aux guerrières pacifiques qui forment cette armée, alors l’idée était
-          lancé, c’était parti. Doucement, au rythme du bouche à oreille et de mes différents apprentissages,
-          l’accompagnement individuel s’est développé et de plus en plus de guerrières pacifiques m’ont offert leur
-          confiance et m’ont accepté dans leur équipe.
-        </p>
-        <p>
-          L’accompagnement individuel rejoint désormais officiellement le Loma Club, l’armée pacifique grandit, se
-          transforme et devient chaque jour un peu plus puissante.
-          Jamais seules, ou seules ensemble, l’armée pacifique est magique.
+         Les suivis se font le plus souvent sur deux mois et demandent un réel investissement de ta part afin que nous puissions rendre cet accompagnement le plus puissant possible. La fréquence de nos sessions et les tarifs de cet accompagnement individuel sont sur mesure.
         </p>
       </section>
       <section class="max-w-prose mx-auto text-lg">
@@ -81,8 +55,9 @@
       <section class="my-6 prose prose-yellow prose-lg text-gray-700 mx-auto">
 
         <p>
-          Important : l’accompagnement individuel ne remplace pas un accompagnement psychologique ou médical quel qu’il
-          soit.
+          <strong>
+            Important : l’accompagnement individuel ne remplace pas un accompagnement psychologique ou médical quel qu’il soit.
+          </strong>
         </p>
       </section>
     </main>
